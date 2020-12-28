366fe6a0e8992691c4de2b6ac02552681fcc6d98
Bob den Otter <bob@twokings.nl>
2020-12-28 16:59:36 +0100

    Working on array merge

--- src/ArrayMerge.php
+++ src/ArrayMerge.php
@@ -8,21 +8,44 @@ class ArrayMerge
 {
     public static function merge(array $arr1, array $arr2): array
     {
-        $result = array_merge_recursive($arr1, $arr2);
-
-        return self::deepUnique($result);
+        return self::mergeDeepArray([$arr1, $arr2]);
     }
 
-    private static function deepUnique(array $data): array
-    {
-        $result = array_map('unserialize', array_unique(array_map('serialize', $data)));
+    /**
+     * @see https://api.drupal.org/api/drupal/vendor%21wikimedia%21composer-merge-plugin%21src%21Merge%21NestedArray.php/function/NestedArray%3A%3AmergeDeepArray/8.7.x
+     */
+    public static function mergeDeepArray(array $arrays, $preserveIntegerKeys = false) {
+        $result = [];
+        foreach ($arrays as $array) {
+            foreach ($array as $key => $value) {
 
-        foreach ($result as $key => $value) {
-            if (\is_array($value)) {
-                $result[$key] = self::deepUnique($value);
+                // Don't add duplicates of objects here
+                if (in_array($value, $result) && is_object($value)) {
+                    continue;
+                }
+                // Renumber integer keys as array_merge_recursive() does
+                // unless $preserveIntegerKeys is set to TRUE. Note that PHP
+                // automatically converts array keys that are integer strings
+                // (e.g., '1') to integers.
+                if (is_integer($key) && !$preserveIntegerKeys) {
+                    $result[] = $value;
+                }
+                elseif (isset($result[$key]) && is_array($result[$key]) && is_array($value)) {
+
+                    // Recurse when both values are arrays.
+                    $result[$key] = self::mergeDeepArray(array(
+                        $result[$key],
+                        $value,
+                    ), $preserveIntegerKeys);
+                }
+                else {
+
+                    // Otherwise, use the latter value, overriding any
+                    // previous value.
+                    $result[$key] = $value;
+                }
             }
         }
-
         return $result;
     }
 }
